ca7dc69383e14093feb0e7cc272b97282c763c58
noeKen <kenfaclnoe@gmail.com>
2025-05-20 11:33:32 -0400

    Ajout de vercel analytics et des liens vers mes reseaux sociaux

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,16 @@
-
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Toaster } from "@/components/ui/toaster";
 import { FloatingElements } from "@/components/animations/FloatingElements";
 import { AnimatePresence } from "framer-motion";
+import { Analytics } from "@vercel/analytics/next";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <AnimatePresence mode="wait">
         <Component {...pageProps} />
+        <Analytics />
       </AnimatePresence>
       <FloatingElements />
       <Toaster />
